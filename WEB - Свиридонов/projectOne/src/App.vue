c101f1bd51fce281be3184704a65d5af750a1c7f
Hansola <reynasio27@gmail.com>
2025-04-05 10:34:31 +0700

    05/04/25

---
+++ WEB - Свиридонов/projectOne/src/App.vue
@@ -0,0 +1,45 @@
+<!-- <script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script> -->
+
+<template>
+<!--<div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" /> -->
+
+  <p>Добро пожаловать</p>
+  <p>Ваш счет: {{ count }}</p>
+  <button @click="clickCount">Нажми на меня</button> <!--присваем фунцию-->
+  
+
+</template>
+
+<script>
+export default {
+  data()
+  {
+    return {
+      abc: "Привет",
+      count: 0,
+      isNatural: true
+    }  
+  },
+
+  methods: {
+    clickCount(){
+      console.log("Меня зажали");
+      this.count++;
+    }
+  }
+}
+</script>
+
+<style scoped> /*scoped все стили будут применимы только для этого файла*/
+
+</style>
