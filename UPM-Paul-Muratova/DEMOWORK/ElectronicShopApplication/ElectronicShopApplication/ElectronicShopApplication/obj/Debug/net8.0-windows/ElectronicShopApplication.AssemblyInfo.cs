c6552b34a9bd74af024b1ac04bd664ea5c7a0a8e
Hansola1 <145135046+Hansola1@users.noreply.github.com>
2025-12-16 23:44:47 +0700

    С# Подготовка к ДЭ (доп)

--- UPM-Paul-Muratova/DEMOWORK/ElectronicShopApplication/ElectronicShopApplication/ElectronicShopApplication/obj/Debug/net8.0-windows/ElectronicShopApplication.AssemblyInfo.cs
+++ UPM-Paul-Muratova/DEMOWORK/ElectronicShopApplication/ElectronicShopApplication/ElectronicShopApplication/obj/Debug/net8.0-windows/ElectronicShopApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ElectronicShopApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85ff56382d5a8809ae480816d0b57bd18063a6af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ff336a3739cfcb63061613b8c312c802e45d5d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ElectronicShopApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ElectronicShopApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
